b6c68d05920517d6f3272e2d9c6f6dcb1152671b
Nishant-Goutam <nishu70555@gmail.com>
2026-01-07 23:52:34 +0530

    small change

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
 
   return (
     <nav className="fixed w-full z-50 h-16 px-6 md:px-10 md:py-3 bg-black/20 backdrop-blur-md text-white">
-      <div className="flex justify-between items-center max-w-7xl mx-auto">
+      <div className="flex justify-between items-center max-w-7xl mx-auto h-full px-6 md:px-10">
         {/* LOGO */}
         <Link href="/" className="flex items-center h-full">
           <div className="relative h-10 w-32">
